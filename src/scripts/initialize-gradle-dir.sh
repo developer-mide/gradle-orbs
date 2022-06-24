2bfa6b7db11a4e660cc9c50c2f6c4fd7255268e9
developer_mide <olumidesamuel588@gmail.com>
2022-06-24 19:36:56 +0100

    commited changes

--- src/scripts/initialize-gradle-dir.sh
+++ src/scripts/initialize-gradle-dir.sh
@@ -5,3 +5,9 @@
 # Gradle tests
 mkdir -p ~/.gradle && cp gradle/init-with-moved-buildDir.gradle ~/.gradle/init.gradle
 
+# generate gradle cache seed
+find . -name 'build.gradle.kts' | sort | xargs cat | shasum | awk '{print $1}' > /tmp/gradle_cache_seed
+
+# restore gradle_cache_seed
+gradle-v1-{{ checksum "/tmp/gradle_cache_seed" }}-{{ checksum ".circleci/config.yml" }}
+
