04102a50de819d80e57dc159d1d4e551225700ac
developer_mide <olumidesamuel588@gmail.com>
2022-06-24 19:26:09 +0100

    commited changes

--- src/scripts/initialize-gradle-dir.sh
+++ src/scripts/initialize-gradle-dir.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+#
+
+# Gradle tests
 mkdir -p ~/.gradle && cp gradle/init-with-moved-buildDir.gradle ~/.gradle/init.gradle
+
---
+++ src/scripts/shallow-checkout.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# SHALLOW_CHECKOUT
+mkdir -p $HOME/.ssh
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+cd ~
+rm -r ~/project
+BRANCH=$CIRCLE_BRANCH
+if [ -z "$BRANCH" ];
+then
+BRANCH=$CIRCLE_TAG
+fi
+git clone --single-branch --depth 1 "$CIRCLE_REPOSITORY_URL" --branch "$BRANCH" ~/project
