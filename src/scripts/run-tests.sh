7fb391cafd26defe7eeb05c30b03e38f1360ce55
developer_mide <olumidesamuel588@gmail.com>
2022-06-24 20:37:33 +0100

    commited changes

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
 gradle :backend:$PROJECT:$TEST_NAME:backend:$PROJECT:writeSuccessfulOutput --build-cache
