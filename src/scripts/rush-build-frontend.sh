8a3fe11bc028ff59eb2561f0025431e8b3b50db5
developer_mide <olumidesamuel588@gmail.com>
2022-06-26 00:52:53 +0100

    commited changes

--- src/scripts/rush-build-frontend.sh
+++ src/scripts/rush-build-frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export BUILD_TIME_STAMP="$(date)"
+BUILD_DATE=""$(date)"
+export BUILD_TIME_STAMP=$BUILD_DATE
 TMP_WORKSPACE_GIT_DESCRIBE=$(cat /tmp/workspace/git/describe)
 export GIT_DESCRIBE=$TMP_WORKSPACE_GIT_DESCRIBE
 RUSH_BUILD_CACHE_CREDENTIAL=$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY node common/scripts/install-run-rush.js build -t @brace/borrower-frontend -t @brace/servicer --verbose
